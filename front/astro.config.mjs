8ba3a6cfe737a1464364e89dcd666e20c8294eb6
sting7401 <78519004+sting7401@users.noreply.github.com>
2026-07-25 14:31:10 +0900

    front

---
+++ front/astro.config.mjs
@@ -0,0 +1,13 @@
+import react from '@astrojs/react';
+import svelte from '@astrojs/svelte';
+import tailwindcss from '@tailwindcss/vite';
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+	vite: {
+		plugins: [tailwindcss()],
+	},
+	integrations: [svelte(), react()],
+});
