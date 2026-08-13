2d4542e75602a4a7bcc9c2fba39ef2d1e15baa1a
songkyoenghwan <sting01@deepfine.ai>
2026-08-13 17:30:27 +0900

    style(ui): 카테고리 색 제어 변경

--- apps/front/src/components/svelte/map/Category.svelte
+++ apps/front/src/components/svelte/map/Category.svelte
@@ -32,7 +32,6 @@
 			<ul
 				class={[
 					'category-scroll scrollbar-hide relative flex h-12 touch-pan-x snap-x snap-proximity items-center gap-2 overflow-x-auto scroll-smooth px-5 transition-all',
-
 					$layoutViewState === 'default' && 'py-2',
 					$layoutViewState === 'ai' && 'py-2',
 					$layoutViewState === 'search' && 'pb-2',
@@ -47,34 +46,42 @@
 						<label
 							for={`category-${category.id}`}
 							class={[
-								'group/category flex items-center gap-1 rounded-sm border px-2 py-1.5 text-sm leading-none transition-colors',
-								category.isEventCategory ? 'border-(--category-color)! bg-(--category-color)/10' : 'bg-white',
-								$layoutViewState === 'default' && 'border-white shadow-2xs',
-								$layoutViewState === 'ai' && 'border-white shadow-2xs',
-								$layoutViewState === 'search' && 'border-slate-200',
+								'group/category flex min-h-8 flex-col items-center gap-1 rounded-lg bg-white text-sm leading-none transition-colors',
+								$layoutViewState === 'default' && 'shadow-2xs',
+								$layoutViewState === 'ai' && 'shadow-2xs',
 							]}
 							style:--category-color={category?.categoryColorCodes?.colorCode}
 						>
-							<Icons
-								name={String(category.iconKey)}
-								cls="size-5 fill-(--category-color) stroke-(--category-color)"
-							/>
-							<input
-								type="radio"
-								name="category-search"
-								id={`category-${category.id}`}
-								class="sr-only"
-								value={category.iconKey}
-								bind:group={$categoryState}
-								onchange={(e: Event) => {
-									const input = e.currentTarget as HTMLInputElement;
+							<div
+								class={[
+									'flex h-full flex-1 items-center gap-1 rounded-lg border px-2 py-1',
+									category.isEventCategory ? 'border-(--category-color)! bg-(--category-color)/10' : 'bg-white',
+									$layoutViewState === 'default' && 'border-white shadow-2xs',
+									$layoutViewState === 'ai' && 'border-white shadow-2xs',
+									$layoutViewState === 'search' && 'border-slate-200',
+								]}
+							>
+								<Icons
+									name={String(category.iconKey)}
+									cls="size-5 fill-(--category-color) stroke-(--category-color)"
+								/>
+								<input
+									type="radio"
+									name="category-search"
+									id={`category-${category.id}`}
+									class="sr-only"
+									value={category.iconKey}
+									bind:group={$categoryState}
+									onchange={(e: Event) => {
+										const input = e.currentTarget as HTMLInputElement;
 
-									if (input.checked) {
-										onCategoryHandler();
-									}
-								}}
-							/>
-							{pickText(category.name, $langState)}
+										if (input.checked) {
+											onCategoryHandler();
+										}
+									}}
+								/>
+								{pickText(category.name, $langState)}
+							</div>
 						</label>
 					</li>
 				{/each}
